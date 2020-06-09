f7d52efc7cae2a3c3c2b29f8d158fa26122cf03d
Alexis Chauvet <chauvetalexis@gmail.com>
2020-06-09 13:15:28 +0200

    Ajout de Firestore

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore/firestore';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,9 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public afFirestore: AngularFirestore) { }
+
+  addMovies() {
+  }
 
 }
