0e19eab4f504671fa12d9b1ab25a84bc0c4d7389
Alexis Chauvet <chauvetalexis@gmail.com>
2020-06-09 12:52:05 +0200

    Installation Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,35 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+///////////////////////FIREBASE///////////////////////
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBqSxb6Q4y1--L11JIrkMOBa6ts22pSQDI",
+  authDomain: "movizz-7a9b3.firebaseapp.com",
+  databaseURL: "https://movizz-7a9b3.firebaseio.com",
+  projectId: "movizz-7a9b3",
+  storageBucket: "movizz-7a9b3.appspot.com",
+  messagingSenderId: "550149167679",
+  appId: "1:550149167679:web:9bf1a5f067c9f1406f368a",
+  measurementId: "G-R6MZXL37RF"
+};
+
+//////////////////////////////////////////////////////
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule],
   providers: [
     StatusBar,
     SplashScreen,
@@ -20,4 +45,5 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
+
